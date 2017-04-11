3863f494cf256bd053c2aed3b769fa03506a3017
Martha Benavides <marthalbm95@hotmail.com>
2017-04-11 13:51:43 -0500

    Archivo para poner la máquina virtual

---
+++ MaquinaVirtualAPODORAX.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------
+#
+#  Jose Gonzalez Ayerdi - A01036121
+#  Martha Benavides - A01280115
+#  Proyecto Final, Diseño de Compiladores
+#  Maquina Virtual APODORAX
+# ------------------------------------------------------------
